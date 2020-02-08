a011b91012878a69ebb97351ee49305aa0785b63
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-02-08 10:54:01 +0000

    - FIXED PLACEMENT ISSUE (Thanks @Raicuparta!) - Cleaned up code - Delete now deletes selected object - Rotation / movement of selected object - Button to toggle fast/slow movement

--- ObjectSpawner/Class1.cs
+++ ObjectSpawner/Class1.cs
@@ -20,11 +20,9 @@ namespace ObjectSpawner
 
         public GameObject edit;
 
+        public bool moveSpeedFast = false;
+        public float moveSpeed = 0.1f;
         public bool editMode = false;
-
-        public List<GameObject> goList = new List<GameObject>();
-
-        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 
         private void Start()
         {
@@ -33,6 +31,9 @@ namespace ObjectSpawner
 
         void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
+            // Create copies of any object we want to spawn. This way we can
+            // edit their properties here, instead of editing them in the spawn
+            // function.
             solanum = Instantiate(GameObject.Find("Character_NOM_Solanum"));
             solanum.SetActive(false);
 
@@ -61,39 +62,22 @@ namespace ObjectSpawner
 
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.KeypadMinus))
-            {
-                NotificationData data = new NotificationData(NotificationTarget.Player, "DELETING " + goList[goList.Count-1].name + " FROM SCENE", 3f, true);
-                NotificationManager.SharedInstance.PostNotification(data, false);
-                Destroy(goList[goList.Count-1]);
-                goList.Remove(goList[goList.Count-1]);
-            }
-
+            // Toggle edit mode
             if (Input.GetKeyDown(KeyCode.KeypadDivide))
             {
-                NotificationData enterEdit = new NotificationData(NotificationTarget.Player, "ENTERING EDIT MODE", 3f, true);
-                NotificationData exitEdit = new NotificationData(NotificationTarget.Player, "EXITING EDIT MODE", 3f, true);
-                if (!editMode)
-                {
-                    editMode = true;
-                    NotificationManager.SharedInstance.PostNotification(enterEdit, false);
-                }
-                else
-                {
-                    editMode = false;
-                    NotificationManager.SharedInstance.PostNotification(exitEdit, false);
-                }
+                editMode = !editMode;
+                NotificationData data = editMode ? new NotificationData(NotificationTarget.Player, "ENTERING EDIT MODE", 3f, true) : new NotificationData(NotificationTarget.Player, "EXITING EDIT MODE", 3f, true);
+                NotificationManager.SharedInstance.PostNotification(data, false);
             }
 
             if (editMode)
             {
                 if (Input.GetKeyDown(KeyCode.KeypadMultiply))
                 {
-                    Vector3 forward = Locator.GetPlayerTransform().forward;
-                    Vector3 forward2 = Locator.GetPlayerCamera().transform.forward;
+                    Vector3 forward = Locator.GetPlayerCamera().transform.forward;
                     RaycastHit hit;
 
-                    if (Physics.Raycast(Locator.GetPlayerCamera().transform.position, forward2, out hit, 100f, OWLayerMask.physicalMask | OWLayerMask.interactMask))
+                    if (Physics.Raycast(Locator.GetPlayerCamera().transform.position, forward, out hit, 100f, OWLayerMask.physicalMask | OWLayerMask.interactMask))
                     {
                         edit = hit.collider.gameObject;
                         UpdateEditHud();
@@ -101,11 +85,71 @@ namespace ObjectSpawner
                     }
                 }
 
-                if (Input.GetKeyDown(KeyCode.PageUp))
+                // Toggle fast/slow movement
+                if (Input.GetKeyDown(KeyCode.ScrollLock))
+                {
+                    moveSpeedFast = !moveSpeedFast;
+                    moveSpeed = moveSpeedFast ? 10f : 0.1f;
+                    NotificationData data = moveSpeedFast ? new NotificationData(NotificationTarget.Player, "FAST MOVEMENT", 3f, true) : new NotificationData(NotificationTarget.Player, "SLOW MOVEMENT", 3f, true);
+                    NotificationManager.SharedInstance.PostNotification(data, false);
+                }
+
+                // Delete selected object
+                if (Input.GetKeyDown(KeyCode.KeypadMinus))
+                {
+                    NotificationData data = new NotificationData(NotificationTarget.Player, "DELETING " + edit.name + " FROM SCENE", 3f, true);
+                    NotificationManager.SharedInstance.PostNotification(data, false);
+                    Destroy(edit);
+                }
+
+                // Select parent
+                if (Input.GetKeyDown(KeyCode.KeypadPeriod))
                 {
                     edit = edit.transform.parent.gameObject;
                     UpdateEditHud();
                     UpdateEditUI();
+                }
+
+                // ROTATION //
+                if (Input.GetKeyDown(KeyCode.Keypad7))
+                {
+                    edit.transform.Rotate(0, 10, 0);
+                }
+
+                if (Input.GetKeyDown(KeyCode.Keypad9))
+                {
+                    edit.transform.Rotate(0, -10, 0);
+                }
+
+                // MOVEMENT //
+                if (Input.GetKeyDown(KeyCode.Keypad4))
+                {
+                    edit.transform.Translate(moveSpeed, 0f, 0f);
+                }
+
+                if (Input.GetKeyDown(KeyCode.Keypad6))
+                {
+                    edit.transform.Translate(-moveSpeed, 0f, 0f);
+                }
+
+                if (Input.GetKeyDown(KeyCode.Keypad2))
+                {
+                    edit.transform.Translate(0f, 0f, moveSpeed);
+                }
+
+                if (Input.GetKeyDown(KeyCode.Keypad8))
+                {
+                    edit.transform.Translate(0f, 0f, -moveSpeed);
+                }
+
+                if (Input.GetKeyDown(KeyCode.KeypadPlus))
+                {
+                    edit.transform.Translate(0f, moveSpeed, 0f);
+                }
+
+                if (Input.GetKeyDown(KeyCode.KeypadEnter))
+                {
+                    edit.transform.Translate(0f, -moveSpeed, 0f);
                 }
             }
             else
@@ -137,22 +181,32 @@ namespace ObjectSpawner
 
         void UpdateEditUI()
         {
+            // Destroy old sphere and create new one
+            Destroy(GameObject.Find("EditModeSphere"));
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.name = "EditModeSphere";
             sphere.transform.parent = edit.transform;
             sphere.transform.localPosition = Vector3.zero;
+            sphere.DestroyAllComponents<Collider>();
+
+            // Set colour and transparency
+            sphere.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+            sphere.GetComponent<MeshRenderer>().material.SetAlpha(0.5f);
         }
 
         void PlaceObject(Vector3 normal, Vector3 point, GameObject gameObject, OWRigidbody targetRigidbody, bool lookAtPlayer)
         {
+            // Get object to parent gameObject to
             Transform parent = targetRigidbody.transform;
             gameObject.SetActive(true);
             gameObject.transform.SetParent(parent);
-            Quaternion lhs = Quaternion.FromToRotation(gameObject.transform.TransformDirection(Vector3.up), normal);
-            gameObject.transform.rotation = lhs * gameObject.transform.rotation;
             gameObject.transform.position = point + gameObject.transform.TransformDirection(Vector3.zero);
+
             if (lookAtPlayer)
             {
-                //gameObject.transform.RotateAround(point, normal, Vector3.Angle(gameObject.transform.forward, Locator.GetPlayerTransform().forward) + 180);
-                gameObject.transform.LookAt(Locator.GetPlayerTransform().transform, normal);
+                // Thanks Rai!
+                var look = Vector3.ProjectOnPlane(Locator.GetPlayerTransform().transform.position - gameObject.transform.position, normal);
+                gameObject.transform.rotation = Quaternion.LookRotation(look, normal);
             }
         }
 
@@ -160,7 +214,6 @@ namespace ObjectSpawner
         {
             if (IsPlaceable(out Vector3 placeNormal, out Vector3 placePoint, out OWRigidbody targetRigidbody))
             {
-                goList.Add(gameObject);
                 PlaceObject(placeNormal, placePoint, gameObject, targetRigidbody, lookAtPlayer);
             }
         }
@@ -171,12 +224,9 @@ namespace ObjectSpawner
             placePoint = Vector3.zero;
             targetRigidbody = null;
 
-            Vector3 forward = Locator.GetPlayerTransform().forward;
-            Vector3 forward2 = Locator.GetPlayerCamera().transform.forward;
+            Vector3 forward = Locator.GetPlayerCamera().transform.forward;
 
-            RaycastHit hit;
-            
-            if (Physics.Raycast(Locator.GetPlayerCamera().transform.position, forward2, out hit, 100f, OWLayerMask.physicalMask | OWLayerMask.interactMask))
+            if (Physics.Raycast(Locator.GetPlayerCamera().transform.position, forward, out RaycastHit hit, 100f, OWLayerMask.physicalMask | OWLayerMask.interactMask))
             {
                 placeNormal = hit.normal;
                 placePoint = hit.point;
