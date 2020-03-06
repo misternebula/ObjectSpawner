8ddcfc3316143c99757c9f88a0456e7cc06a400e
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2020-03-06 00:09:04 +0000

    Rewrite of asset loading

--- ObjectSpawner/Class1.cs
+++ ObjectSpawner/Class1.cs
@@ -1,4 +1,5 @@
-﻿using OWML.ModHelper;
+﻿using OWML.Common;
+using OWML.ModHelper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,9 @@ namespace ObjectSpawner
 {
     public class MainClass : ModBehaviour
     {
-        public GameObject solanum;
-        public GameObject esker;
-        public GameObject campfire;
-        public GameObject tektite;
-        public GameObject reibeck;
-        public GameObject mica;
-        public GameObject NOMstaff;
+        public const int EXPECTED_GO_COUNT = 5;
+
+        public GameObject[] gameObjects = new GameObject[0];
 
         public GameObject edit;
 
@@ -24,8 +21,22 @@ namespace ObjectSpawner
         public float moveSpeed = 0.1f;
         public bool editMode = false;
 
+        public AssetBundle _assetBundle;
+
+        public string copy;
+
+        public static IModHelper helper;
+
+        public static bool inputEnabled = false;
         private void Start()
         {
+            base.ModHelper.Console.WriteLine("[ObjectSpawner] :");
+            _assetBundle = base.ModHelper.Assets.LoadBundle("selectionbundle");
+            helper = base.ModHelper;
+            if (_assetBundle != null)
+            {
+                base.ModHelper.Console.WriteLine(":     Assetbundle loaded successfully.");
+            }
             SceneManager.sceneLoaded += OnSceneLoaded;
         }
 
@@ -34,141 +45,194 @@ namespace ObjectSpawner
             // Create copies of any object we want to spawn. This way we can
             // edit their properties here, instead of editing them in the spawn
             // function.
-            solanum = Instantiate(GameObject.Find("Character_NOM_Solanum"));
-            solanum.SetActive(false);
+            base.ModHelper.Console.WriteLine(":     ObjectSpawner GO load...");
 
-            mica = Instantiate(GameObject.Find("Villager_HEA_Mica"));
-            mica.SetActive(false);
+            Array.Resize<GameObject>(ref gameObjects, gameObjects.Length + 1);
+            gameObjects[gameObjects.Length - 1] = Instantiate(GameObject.Find("Character_NOM_Solanum"));
 
-            NOMstaff = Instantiate(GameObject.Find("Prefab_NOM_Staff"));
-            NOMstaff.SetActive(false);
+            Array.Resize<GameObject>(ref gameObjects, gameObjects.Length + 1);
+            gameObjects[gameObjects.Length - 1] = Instantiate(GameObject.Find("Villager_HEA_Mica"));
 
-            /*
-            esker = Instantiate(GameObject.Find("Villager_HEA_Esker"));
-            esker.SetActive(false);
-            StreamingManager.LoadStreamingAssets("timberhearth/meshes/characters");
+            Array.Resize<GameObject>(ref gameObjects, gameObjects.Length + 1);
+            gameObjects[gameObjects.Length - 1] = Instantiate(GameObject.Find("Prefab_NOM_Staff"));
 
-            campfire = Instantiate(GameObject.Find("Effects_HEA_Campfire"));
-            campfire.SetActive(false);
+            Array.Resize<GameObject>(ref gameObjects, gameObjects.Length + 1);
+            gameObjects[gameObjects.Length - 1] = Instantiate<GameObject>(GameObject.Find("Prefab_NOM_Torch"));
 
-            tektite = Instantiate(GameObject.Find("Villager_HEA_Tektite_2"));
-            tektite.SetActive(false);
+            Array.Resize<GameObject>(ref gameObjects, gameObjects.Length + 1);
+            gameObjects[gameObjects.Length - 1] = Instantiate(GameObject.Find("EscapePodFlare_Body"));
 
-            reibeck = Instantiate(GameObject.Find("Traveller_HEA_Riebeck"));
-            reibeck.SetActive(false);
-            StreamingManager.LoadStreamingAssets("brittlehollow/meshes/characters");
-            */
+            foreach (var item in gameObjects)
+            {
+                item.SetActive(false);
+            }
+
+            base.ModHelper.Console.WriteLine(":     Successfully loaded [" + gameObjects.Length + "] GameObjects.");
+
+            if (gameObjects.Length != EXPECTED_GO_COUNT)
+            {
+                base.ModHelper.Console.WriteLine(":     ERROR! Expected number of GOs is different than loaded!");
+            }
         }
 
         void Update()
         {
-            // Toggle edit mode
-            if (Input.GetKeyDown(KeyCode.KeypadDivide))
+            if (inputEnabled)
             {
-                editMode = !editMode;
-                NotificationData data = editMode ? new NotificationData(NotificationTarget.Player, "ENTERING EDIT MODE", 3f, true) : new NotificationData(NotificationTarget.Player, "EXITING EDIT MODE", 3f, true);
-                NotificationManager.SharedInstance.PostNotification(data, false);
-            }
-
-            if (editMode)
-            {
-                if (Input.GetKeyDown(KeyCode.KeypadMultiply))
+                // Toggle edit mode
+                if (Input.GetKeyDown(KeyCode.KeypadDivide))
                 {
-                    Vector3 forward = Locator.GetPlayerCamera().transform.forward;
-                    RaycastHit hit;
-
-                    if (Physics.Raycast(Locator.GetPlayerCamera().transform.position, forward, out hit, 100f, OWLayerMask.physicalMask | OWLayerMask.interactMask))
+                    editMode = !editMode;
+                    NotificationData data = editMode ? new NotificationData(NotificationTarget.Player, "ENTERING EDIT MODE", 3f, true) : new NotificationData(NotificationTarget.Player, "EXITING EDIT MODE", 3f, true);
+                    NotificationManager.SharedInstance.PostNotification(data, false);
+                    if (!editMode)
                     {
-                        edit = hit.collider.gameObject;
-                        UpdateEditHud();
-                        UpdateEditUI();
+                        Destroy(GameObject.Find("EditModeSphere"));
                     }
                 }
 
-                // Toggle fast/slow movement
-                if (Input.GetKeyDown(KeyCode.ScrollLock))
+                if (editMode)
                 {
-                    moveSpeedFast = !moveSpeedFast;
-                    moveSpeed = moveSpeedFast ? 10f : 0.1f;
-                    NotificationData data = moveSpeedFast ? new NotificationData(NotificationTarget.Player, "FAST MOVEMENT", 3f, true) : new NotificationData(NotificationTarget.Player, "SLOW MOVEMENT", 3f, true);
-                    NotificationManager.SharedInstance.PostNotification(data, false);
-                }
+                    if (Input.GetKeyDown(KeyCode.KeypadMultiply))
+                    {
+                        Vector3 forward = Locator.GetPlayerCamera().transform.forward;
+                        RaycastHit hit;
 
-                // Delete selected object
-                if (Input.GetKeyDown(KeyCode.KeypadMinus))
-                {
-                    NotificationData data = new NotificationData(NotificationTarget.Player, "DELETING " + edit.name + " FROM SCENE", 3f, true);
-                    NotificationManager.SharedInstance.PostNotification(data, false);
-                    Destroy(edit);
-                }
+                        if (Physics.Raycast(Locator.GetPlayerCamera().transform.position, forward, out hit, 100f, OWLayerMask.physicalMask | OWLayerMask.interactMask))
+                        {
+                            edit = hit.collider.gameObject;
+                            UpdateEditHud();
+                            UpdateEditUI();
+                        }
+                    }
 
-                // Select parent
-                if (Input.GetKeyDown(KeyCode.KeypadPeriod))
-                {
-                    edit = edit.transform.parent.gameObject;
-                    UpdateEditHud();
-                    UpdateEditUI();
-                }
+                    // Toggle fast/slow movement
+                    if (Input.GetKeyDown(KeyCode.ScrollLock))
+                    {
+                        moveSpeedFast = !moveSpeedFast;
+                        moveSpeed = moveSpeedFast ? 10f : 0.1f;
+                        NotificationData data = moveSpeedFast ? new NotificationData(NotificationTarget.Player, "FAST MOVEMENT", 3f, true) : new NotificationData(NotificationTarget.Player, "SLOW MOVEMENT", 3f, true);
+                        NotificationManager.SharedInstance.PostNotification(data, false);
+                    }
 
-                // ROTATION //
-                if (Input.GetKeyDown(KeyCode.Keypad7))
-                {
-                    edit.transform.Rotate(0, 10, 0);
-                }
+                    // Delete selected object
+                    if (Input.GetKeyDown(KeyCode.KeypadMinus))
+                    {
+                        NotificationData data = new NotificationData(NotificationTarget.Player, "DELETING " + edit.name + " FROM SCENE", 3f, true);
+                        NotificationManager.SharedInstance.PostNotification(data, false);
+                        Destroy(edit);
+                    }
 
-                if (Input.GetKeyDown(KeyCode.Keypad9))
-                {
-                    edit.transform.Rotate(0, -10, 0);
-                }
+                    // Select parent
+                    if (Input.GetKeyDown(KeyCode.KeypadPeriod))
+                    {
+                        edit = edit.transform.parent.gameObject;
+                        UpdateEditHud();
+                        UpdateEditUI();
+                    }
 
-                // MOVEMENT //
-                if (Input.GetKeyDown(KeyCode.Keypad4))
-                {
-                    edit.transform.Translate(moveSpeed, 0f, 0f);
-                }
+                    // ROTATION //
+                    if (Input.GetKeyDown(KeyCode.Keypad7))
+                    {
+                        edit.transform.Rotate(0, 10, 0);
+                    }
 
-                if (Input.GetKeyDown(KeyCode.Keypad6))
-                {
-                    edit.transform.Translate(-moveSpeed, 0f, 0f);
-                }
+                    if (Input.GetKeyDown(KeyCode.Keypad9))
+                    {
+                        edit.transform.Rotate(0, -10, 0);
+                    }
 
-                if (Input.GetKeyDown(KeyCode.Keypad2))
-                {
-                    edit.transform.Translate(0f, 0f, moveSpeed);
-                }
+                    if (Input.GetKeyDown(KeyCode.UpArrow))
+                    {
+                        edit.transform.Rotate(-10, 0, 0);
+                    }
 
-                if (Input.GetKeyDown(KeyCode.Keypad8))
-                {
-                    edit.transform.Translate(0f, 0f, -moveSpeed);
-                }
+                    if (Input.GetKeyDown(KeyCode.DownArrow))
+                    {
+                        edit.transform.Rotate(10, 0, 0);
+                    }
 
-                if (Input.GetKeyDown(KeyCode.KeypadPlus))
-                {
-                    edit.transform.Translate(0f, moveSpeed, 0f);
-                }
+                    if (Input.GetKeyDown(KeyCode.LeftArrow))
+                    {
+                        edit.transform.Rotate(0, 0, -10);
+                    }
 
-                if (Input.GetKeyDown(KeyCode.KeypadEnter))
-                {
-                    edit.transform.Translate(0f, -moveSpeed, 0f);
-                }
-            }
-            else
-            {
-                if (Input.GetKeyDown(KeyCode.Keypad0))
-                {
-                    PlaceObjectRaycast(Instantiate(solanum), true);
-                    StreamingManager.LoadStreamingAssets("quantummoon/meshes/characters");
-                }
+                    if (Input.GetKeyDown(KeyCode.RightArrow))
+                    {
+                        edit.transform.Rotate(0, 0, 10);
+                    }
 
-                if (Input.GetKeyDown(KeyCode.Keypad1))
-                {
-                    PlaceObjectRaycast(Instantiate(mica), true);
-                }
+                    // MOVEMENT //
+                    if (Input.GetKeyDown(KeyCode.Keypad4))
+                    {
+                        edit.transform.Translate(moveSpeed, 0f, 0f);
+                    }
 
-                if (Input.GetKeyDown(KeyCode.Keypad2))
+                    if (Input.GetKeyDown(KeyCode.Keypad6))
+                    {
+                        edit.transform.Translate(-moveSpeed, 0f, 0f);
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Keypad2))
+                    {
+                        edit.transform.Translate(0f, 0f, moveSpeed);
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Keypad8))
+                    {
+                        edit.transform.Translate(0f, 0f, -moveSpeed);
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.KeypadPlus))
+                    {
+                        edit.transform.Translate(0f, moveSpeed, 0f);
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.KeypadEnter))
+                    {
+                        edit.transform.Translate(0f, -moveSpeed, 0f);
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Keypad1))
+                    {
+                        copy = edit.name;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Keypad3))
+                    {
+                        PlaceObjectRaycast(Instantiate(GameObject.Find(copy)), true);
+                    }
+                }
+                else
                 {
-                    PlaceObjectRaycast(Instantiate(NOMstaff), true);
-                    StreamingManager.LoadStreamingAssets("brittlehollow/meshes/props");
+                    if (Input.GetKeyDown(KeyCode.Keypad0))
+                    {
+                        PlaceObjectRaycast(Instantiate(gameObjects[0]), true);
+                        StreamingManager.LoadStreamingAssets("quantummoon/meshes/characters");
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Keypad1))
+                    {
+                        PlaceObjectRaycast(Instantiate(gameObjects[1]), true);
+                        StreamingManager.LoadStreamingAssets("timberhearth/meshes/characters");
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Keypad2))
+                    {
+                        PlaceObjectRaycast(Instantiate(gameObjects[2]), true);
+                        StreamingManager.LoadStreamingAssets("brittlehollow/meshes/props");
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Keypad3))
+                    {
+                        PlaceObjectRaycast(Instantiate(gameObjects[3]), true);
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.Keypad4))
+                    {
+                        PlaceObjectRaycast(Instantiate(gameObjects[4]), true);
+                        StreamingManager.LoadStreamingAssets("darkbramble/meshes/props");
+                    }
                 }
             }
         }
@@ -183,15 +247,17 @@ namespace ObjectSpawner
         {
             // Destroy old sphere and create new one
             Destroy(GameObject.Find("EditModeSphere"));
-            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            GameObject sphere = Instantiate(_assetBundle.LoadAsset<GameObject>("sphere"));
             sphere.name = "EditModeSphere";
             sphere.transform.parent = edit.transform;
             sphere.transform.localPosition = Vector3.zero;
-            sphere.DestroyAllComponents<Collider>();
+            sphere.DestroyAllComponents<SphereCollider>();
 
-            // Set colour and transparency
-            sphere.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
-            sphere.GetComponent<MeshRenderer>().material.SetAlpha(0.5f);
+            sphere.transform.localScale = edit.GetComponent<Collider>().bounds.size;
+            if (edit.GetComponent<SkinnedMeshRenderer>() != null)
+            {
+                sphere.transform.localScale = edit.GetComponent<SkinnedMeshRenderer>().bounds.size;
+            }
         }
 
         void PlaceObject(Vector3 normal, Vector3 point, GameObject gameObject, OWRigidbody targetRigidbody, bool lookAtPlayer)
@@ -207,6 +273,12 @@ namespace ObjectSpawner
                 // Thanks Rai!
                 var look = Vector3.ProjectOnPlane(Locator.GetPlayerTransform().transform.position - gameObject.transform.position, normal);
                 gameObject.transform.rotation = Quaternion.LookRotation(look, normal);
+            }
+
+            if (gameObject.GetComponentInChildren<OWCollider>() != null)
+            {
+                gameObject.GetComponentInChildren<OWCollider>().SetActivation(true);
+                gameObject.GetComponentInChildren<OWCollider>().enabled = true;
             }
         }
 
@@ -234,6 +306,16 @@ namespace ObjectSpawner
                 return true;
             }
             return false;
+        }
+
+        public static void MNActivateInput()
+        {
+            inputEnabled = true;
+        }
+
+        public static void MNDeactivateInput()
+        {
+            inputEnabled = false;
         }
     }
 }
